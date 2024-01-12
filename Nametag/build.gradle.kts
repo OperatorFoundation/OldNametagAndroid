e21138a9c972ab9c921d420f3444f39e9a3554a3
bragelbytes <joebragel@gmail.com>
2024-01-12 15:33:13 -0800

    Update build.gradle.kts

--- Nametag/build.gradle.kts
+++ Nametag/build.gradle.kts
@@ -35,9 +35,9 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
-    implementation("com.github.operatorfoundation:keychainandroid:3.0.1")
-    implementation("com.github.OperatorFoundation:TransmissionAndroid:1.2.9")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.github.operatorfoundation:keychainandroid:4.0.0")
+    implementation("com.github.operatorfoundation:transmissionandroid:1.3.1")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
