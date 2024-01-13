3a0337787e6d7c51b21c0320035c4b0a4ebc5d94
bragelbytes <joebragel@gmail.com>
2024-01-12 16:07:59 -0800

    Update Nametag.kt

--- Nametag/src/main/java/org/operatorfoundation/nametagandroid/Nametag.kt
+++ Nametag/src/main/java/org/operatorfoundation/nametagandroid/Nametag.kt
@@ -2,14 +2,13 @@ package org.operatorfoundation.nametagandroid
 
 import android.provider.ContactsContract.Data
 import org.operatorfoundation.keychainandroid.KeyType
-
+import org.operatorfoundation.keychainandroid.Keychain
 import org.operatorfoundation.keychainandroid.PublicKey
-import org.operatorfoundation.keychainandroid.
+import org.operatorfoundation.keychainandroid.Signature
+import org.operatorfoundation.keychainandroid.SignatureType
 import org.operatorfoundation.keychainandroid.PrivateKey
 import org.operatorfoundation.transmission.Connection
-import org.operatorfoundation.transmission.Transmission
 import org.operatorfoundation.transmission.TransmissionConnection
-import java.security.Signature
 import kotlin.random.Random
 
 class Nametag {
@@ -19,8 +18,10 @@ class Nametag {
 
     fun check(challenge: ByteArray, clientPublicKey: PublicKey, signature: Signature)
     {
-        // FIXME: isValidSignature is not yet implemented in KeychainAndroid
-        clientPublicKey.isValidSignature(signature, challenge) ?: throw Exception(NametagError.verificationFailed.toString())
+        if (!clientPublicKey.isValidSignature(signature, challenge))
+        {
+            throw Exception(NametagError.verificationFailed.toString())
+        }
     }
 
     fun checkLive(connection: TransmissionConnection): PublicKey
@@ -32,56 +33,99 @@ class Nametag {
 
         if (!wroteChallenge)
         {
-            throw error(NametagError.writeFailed)
+            throw Exception(NametagError.writeFailed.toString())
         }
 
         val signatureData = connection.read(expectedSignatureSize) ?: throw Exception(NametagError.noSignatureReceived.toString())
 
-        val signature = try {
-            Signature(SignatureType.P256, signatureData)
+        // FIXME: Did I do this correctly?
+        val signature = Signature.P256(signatureData)
+
+        try {
+            check(challenge, clientPublicKey, signature)
+        }
+        catch (error: Error)
+        {
+            throw error
+        }
+
+        return clientPublicKey
+    }
+
+    // TODO: Use KeychainLibrary
+    lateinit var privateKey: PrivateKey
+    lateinit var publicKey:  PublicKey
+
+    var data = publicKey.data
+
+    init {
+        val keychain = Keychain()
+        val privateSigningKey = keychain.retrieveOrGeneratePrivateKey("Nametag", KeyType.P256Signing)
+        if (privateSigningKey != null) {
+            this.privateKey = privateSigningKey
+            this.publicKey = privateSigningKey.
+        }
+    }
+
+    fun prove(challenge: ByteArray): Signature
+    {
+        return try {
+            this.privateKey.signatureForData(challenge)
         }
         catch (error: Exception)
         {
             throw error
         }
-
-        return
     }
-
-
-    // TODO: Use KeychainLibrary
-    val privateKey: PrivateKey = TODO()
-
-    public val publicKey: PublicKey
-
-    var data = publicKey.data
-
-    init {
-        val privateSigningKey = keychain.retrieveOrGeneratePrivateKey("Nametag", KeyType.P256Signing) ?: null
-        if (privateSigningKey != null) {
-            this.privateKey = privateSigningKey
-            this.publicKey = privateSigningKey.publicKey
-        }
-    }
-
-//    fun prove(challenge: Data): Signature {
-//
-//        return try {
-//            this.privateKey.signature(for (challenge))
-//        }
-//    }
 
     fun proveLive(connection: Connection)
     {
+        val publicKeyData = publicKey.data ?: throw Exception(NametagError.nilPublicKey.toString())
 
+        if (publicKeyData.size != expectedPublicKeySize)
+        {
+            throw Exception("Received a public key of ${publicKeyData.size} bytes, expected $expectedPublicKeySize bytes.")
+        }
+
+        try {
+            connection.write(publicKeyData)
+        }
+        catch (error: NametagException)
+        {
+            throw Exception(NametagError.writeFailed.toString())
+        }
+
+        val challenge = connection.read(challengeSize) ?: throw Exception(NametagError.noChallengeReceived.toString())
+        // FIXME: Do I need a try here? See NametagSwift
+        val result = prove(challenge)
+        val resultData = result.data
+
+        if (resultData.size != expectedSignatureSize)
+        {
+            throw Exception(NametagError.challengeResultWrongSize.toString())
+        }
+
+        try {
+            connection.write(resultData)
+        }
+        catch (error: NametagException)
+        {
+            throw Exception(NametagError.writeFailed.toString())
+        }
     }
 
     fun endorse(data: ByteArray): Signature {
-        throw Exception("endorse(data:) not yet implemented")
+        return try {
+            privateKey.signatureForData(data)
+        }
+        catch (error: Exception)
+        {
+            throw error
+        }
     }
-
-    fun verify(signature: Signature, data: ByteArray): Boolean {
-        return false
+    fun verify(signature: Signature, data: ByteArray): Boolean
+    {
+        return publicKey.isValidSignature(signature, data)
     }
 
     enum class NametagError(val error: String)
@@ -90,7 +134,7 @@ class Nametag {
         challengeResultWrongSize("Challenge result was the wrong size."),
         writeFailed("Write failed"),
         nilPublicKey("The public key cannot be nil."),
-//        publicKeyWrongSize(receivedSize: Int, expectedSize: Int)
+//        publicKeyWrongSize(receivedSize: Int, expectedSize: Int),
         verificationFailed("The verification failed."),
         noPublicKeyReceived("A public key was not recieved."),
         noSignatureReceived("A signature was not recieved.")
