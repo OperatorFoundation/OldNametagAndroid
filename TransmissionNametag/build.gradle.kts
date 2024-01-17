dd3f24fbc19bf3f7a9ec121a71ef5bf23168a554
consuelita <adelita@operatorfoundation.org>
2024-01-17 12:13:12 -0600

    Update build.gradle.kts

--- TransmissionNametag/build.gradle.kts
+++ TransmissionNametag/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         }
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
