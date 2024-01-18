c61dcb5b7c95953640e9db6919b4bfb08d09e09a
consuelita <adelita@operatorfoundation.org>
2024-01-17 18:47:43 -0600

    Dependencies

--- Nametag/build.gradle.kts
+++ Nametag/build.gradle.kts
@@ -36,9 +36,9 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.9.0")
     implementation("com.github.operatorfoundation:KeychainAndroid:KeychainTypesReArch-SNAPSHOT")
-    implementation("com.github.operatorfoundation:TransmissionAndroid:1.3.1")
+    implementation("com.github.operatorfoundation:TransmissionAndroid:1.3.2")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
