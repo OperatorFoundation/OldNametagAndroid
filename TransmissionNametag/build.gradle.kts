bfa8901bcc3f30da430bf5ea02ec4cc19c3472f7
consuelita <adelita@operatorfoundation.org>
2024-01-23 15:34:06 -0600

    Dependencies

--- TransmissionNametag/build.gradle.kts
+++ TransmissionNametag/build.gradle.kts
@@ -69,8 +69,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.multidex:multidex:2.0.1")
     implementation("com.google.android.material:material:1.9.0")
-    implementation("com.github.operatorfoundation:KeychainAndroid:5.0.0")
-    implementation("com.github.operatorfoundation:TransmissionAndroid:1.3.2")
+    implementation("com.github.operatorfoundation:KeychainAndroid:5.0.1")
+    implementation("com.github.operatorfoundation:TransmissionAndroid:1.3.3")
     implementation(project(":Nametag"))
 
     testImplementation("junit:junit:4.13.2")
