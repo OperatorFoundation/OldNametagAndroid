01cfc77c273de1ee326abc9f00dbf880a13fadda
bragelbytes <joebragel@gmail.com>
2024-02-05 17:09:34 -0800

    update gradle Nametag and TransmissionNametag

--- TransmissionNametag/build.gradle.kts
+++ TransmissionNametag/build.gradle.kts
@@ -41,7 +41,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
 
-    implementation("com.github.operatorfoundation:KeychainAndroid:5.0.1")
+    implementation("com.github.operatorfoundation:KeychainAndroid:5.0.9")
     implementation("com.github.operatorfoundation:TransmissionAndroid:1.3.3")
     implementation(project(":Nametag"))
 
